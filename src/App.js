26a84841e87678ddb5b9434cd9571b98593a13a4
Divij Singh <divij.singhuipath.com@Divijs-MacBook-Pro.local>
2023-06-05 18:14:23 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import './App.css';
+// import style from './Button.module.css'; 
+// import React from 'react';
+
+import React from 'react';
+import { FaBeer } from 'react-icons/fa';
+import './App.css';  // Importing our styles
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FaBeer className="icon red" />
+      <FaBeer className="icon1 blue" />
+      <FaBeer className="icon green" />
     </div>
   );
 }
 
 export default App;
+
+
+
+// export default App;
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
